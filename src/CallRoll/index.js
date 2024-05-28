61e293c5fa792fed2e3099a9376be5f2b0602b59
wangzihaoran <160588135@qq.com>
2024-05-28 11:53:50 +0800

    自动格式化

--- src/CallRoll/index.js
+++ src/CallRoll/index.js
@@ -1,75 +1,82 @@
-import {useNavigate, useParams} from "react-router-dom";
-import React, {useState} from "react";
-import useSWR from "swr";
-import {Button, TabPane, Tabs, Typography} from "@douyinfe/semi-ui";
-import {IconArrowLeft, IconEdit, IconFile} from "@douyinfe/semi-icons";
-import styles from "./index.module.css";
-import {CallRollTab} from "./callRollTab";
-import {MakeGroupTab} from "./makeGroupTab";
-import { LeaveIcon as Leave } from "../atoms/index";
-
+import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import useSWR from 'swr';
+import { Button, TabPane, Tabs, Typography } from '@douyinfe/semi-ui';
+import { IconArrowLeft, IconEdit, IconFile } from '@douyinfe/semi-icons';
+import styles from './index.module.css';
+import { CallRollTab } from './callRollTab';
+import { MakeGroupTab } from './makeGroupTab';
+import { LeaveIcon as Leave } from '../atoms/index';
 
 function App() {
-    const course = useParams().courseId
-    const [changePointsButton, setChangePointsButton] = useState(false) //控制是否修改数据
-    const [changeAttendance, setChangeAttendance] = useState(false)
-    const {mutate} = useSWR(`http://localhost:4000/${course}`)
-    const {Title, Text} = Typography;
+  const course = useParams().courseId;
+  const [changePointsButton, setChangePointsButton] = useState(false); //控制是否修改数据
+  const [changeAttendance, setChangeAttendance] = useState(false);
+  const { mutate } = useSWR(`http://localhost:4000/${course}`);
+  const { Title, Text } = Typography;
 
-    const handleChangeAttendance = ()=>{
-        setChangeAttendance(false)
-    }
-    return (
-        <>
-            <header>
+  const handleChangeAttendance = () => {
+    setChangeAttendance(false);
+  };
+  return (
+    <>
+      <header>
+        <Leave path={`/CourseDetails/${course}`} />
 
-                <Leave path={`/CourseDetails/${course}`}/>
+        <Button
+          onClick={() => {
+            setChangePointsButton(!changePointsButton);
+            mutate();
+          }}
+          type="secondary"
+        >
+          <IconEdit />
+          {changePointsButton ? '确认分数' : '修改分数'}
+        </Button>
 
-                <Button
-                    onClick={() => {
-                    setChangePointsButton(!changePointsButton)
-                    mutate()
-                }} type="secondary"><IconEdit/>{changePointsButton ? '确认分数' : '修改分数'}</Button>
+        <Button
+          onClick={() => {
+            setChangeAttendance(true);
+          }}
+          type="secondary"
+        >
+          <IconFile />
+          修改考勤
+        </Button>
 
-                <Button onClick={()=>{
-                            setChangeAttendance(true)}}
-                        type="secondary">
-                    <IconFile />修改考勤
-                </Button>
+        <Title style={{ margin: '8px 0' }}> {course} </Title>
+      </header>
+      <modal
+        title="添加考勤"
+        okText={'确认修改'}
+        visible={changeAttendance}
+        onOk={handleChangeAttendance}
+        onCancel={() => setChangeAttendance(false)}
+        closeOnEsc={true}
+        maskClosable={false}
+        motion
+      ></modal>
+      <Tabs className={styles.Tabs} type="line">
+        <TabPane tab={'点名'} itemKey={'1'}>
+          <div className={styles.tabChild}>
+            <CallRollTab
+              course={course}
+              changePointsButton={changePointsButton}
+            />
+          </div>
+        </TabPane>
 
-                <Title style={{margin: '8px 0'}}> {course} </Title>
-            </header>
-            <modal
-                title="添加考勤"
-                okText={'确认修改'}
-                visible={changeAttendance}
-                onOk={handleChangeAttendance}
-                onCancel={()=>setChangeAttendance(false)}
-                closeOnEsc={true}
-                maskClosable={false}
-                motion>
-
-            </modal>
-                <Tabs className={styles.Tabs} type="line">
-                    <TabPane tab={'点名'} itemKey={'1'} >
-                        <div className={styles.tabChild}>
-                            <CallRollTab
-                            course={course}
-                            changePointsButton={changePointsButton}
-                            />
-                        </div>
-                    </TabPane>
-
-                    <TabPane tab={'分组'} itemKey={'2'}>
-                        <div className={styles.tabChild}>
-                            <MakeGroupTab course={course} changePointsButton={changePointsButton} />
-                        </div>
-                    </TabPane>
-                </Tabs>
-        </>
-    )
-
-
+        <TabPane tab={'分组'} itemKey={'2'}>
+          <div className={styles.tabChild}>
+            <MakeGroupTab
+              course={course}
+              changePointsButton={changePointsButton}
+            />
+          </div>
+        </TabPane>
+      </Tabs>
+    </>
+  );
 }
 
 export default App;
